491741783ec26043bf62d9857b96780e94493f82
Reka Kupsza <khusire@gmail.com>
2023-07-09 20:42:34 +0200

    create app

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+    /*
+      remotePatterns: [
+          {
+              protocol: "https",
+              hostname: "lh3.googleusercontent.com",
+              port: "",
+          },
+      ],
+  }, */
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  }
+}
+
+module.exports = nextConfig
